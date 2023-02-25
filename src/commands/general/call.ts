b18e01e87e1bb3910a686c9da14ba9a26ab447b8
Jason Wang <jasonywang0@gmail.com>
2023-02-25 15:51:05 -0800

    Switch call to subcommand

--- src/commands/general/call.ts
+++ src/commands/general/call.ts
@@ -4,16 +4,17 @@ import { CommandClass } from '../../structures/command.js';
 export default new CommandClass({
     data: new SlashCommandBuilder()
         .setName('call')
-        .setDescription('Messages drafters')
-        .addStringOption(option => 
-          option
-            .setName('role')
-            .setDescription('Users with role')
-            .setRequired(true)
-            .addChoices(
-              { name: 'Xmage', value: 'xmage' },
-              { name: 'Cockatrice', value: 'cockatrice' },
-				)) as SlashCommandBuilder,
+        .setDescription('Alert drafters')
+        .addSubcommand(subcommand => 
+          subcommand
+            .setName('xmage')
+            .setDescription('Alert Xmage users')
+         )
+         .addSubcommand(subcommand =>
+          subcommand
+            .setName('cockatrice')
+            .setDescription('Alert Cockatrice users')
+         ) as SlashCommandBuilder,
     opt: {
         userPermissions: ['SendMessages'],
         botPermissions: ['SendMessages'],
@@ -24,13 +25,14 @@ export default new CommandClass({
         channels: [process.env.DRAFT_CHANNEL]
     },
     async execute(interaction: ChatInputCommandInteraction<'cached'>) {
+      const subcommand = interaction.options.getSubcommand();
       let content = 'This command can only be used in the draft channel.';
       let ephemeral = true;
       if (this.opt.channels?.includes(interaction.channelId)) {
-        const roleId = interaction.options.getString('role', true) === 'xmage' ? process.env.XMAGE_ROLE : process.env.TRICE_ROLE;
+        const roleId = subcommand === 'xmage' ? process.env.XMAGE_ROLE : process.env.TRICE_ROLE;
         content = `**<@!${interaction.user.id}> calls for <@&${roleId}> to assemble!**`;
         ephemeral = false;
-      }       
+      }
       await interaction.reply({
         content,
         ephemeral,
