7628dc91fbfb12242e3ce39fe29c00891a7f2ad2
Jason Wang <jasonywang0@gmail.com>
2023-02-25 19:38:48 -0800

    Add cube command

---
+++ src/commands/general/cube.ts
@@ -0,0 +1,95 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { CommandClass } from '../../structures/command.js';
+import User from '../../db/models/user.model.js';
+
+export default new CommandClass({
+    data: new SlashCommandBuilder()
+        .setName('cube')
+        .setDescription('Level Info')
+        .addSubcommand(subcommand => 
+          subcommand
+            .setName('get')
+            .setDescription('Get a user\'s cubes')
+            .addUserOption(option =>
+              option
+                .setName('user')
+                .setDescription('Discord user')
+                .setRequired(true)
+              )
+        ) 
+        .addSubcommand(subcommand => 
+          subcommand
+            .setName('add')
+            .setDescription('Add cubes')
+            .addStringOption(option =>
+              option
+                .setName('url')
+                .setDescription('Cube URL')
+                .setRequired(true)
+              )
+        )
+        .addSubcommand(subcommand => 
+          subcommand
+            .setName('delete')
+            .setDescription('Delete cubes')
+            .addStringOption(option =>
+              option
+                .setName('url')
+                .setDescription('Cube URL')
+                .setRequired(true)
+              )
+         ) as SlashCommandBuilder,
+    opt: {
+        userPermissions: ['SendMessages'],
+        botPermissions: ['SendMessages'],
+        category: 'General',
+        cooldown: 5,
+        visible: true,
+        guildOnly: true,
+    },
+    async execute(interaction: ChatInputCommandInteraction<'cached'>) {
+      let content = 'Something went wrong!';
+      try {
+        const subcommand = interaction.options.getSubcommand();
+        if (subcommand === 'get') {
+          let dsUser = interaction.options.getUser('user');
+          if (!dsUser) throw new Error('User could not found be in guild!');
+          let user = await User.findUser(dsUser.id);
+          const cubes = user?.getCubes() || [];
+          if (!cubes.length) throw new Error('The user has no cubes set.');
+          // TODO: Make this look nice
+          content = cubes.toString();            
+        } else {
+          let user = await User.findUser(interaction.user.id);
+          if (!user) user = new User({discordId: interaction.user.id});
+          let option = interaction.options.getString('url');
+          if (subcommand === 'add') {
+            const cubes = user.getCubes();
+            if (cubes.length > 5) throw new Error('the limt has been set');
+            user.addCube(option);
+            const url = new URL(option);
+            if (url.hostname !== 'cubecobra.com') throw new Error(`URL must be a complete link from cube cobra. Here's an example: https://cubecobra.com/cube/list/thunderwang`);
+            await user.save();
+            content = `${option} has been added!`;
+          } else if (subcommand === 'delete') {
+            const cube = user.findCube(option);
+            if (!cube) throw new Error('Cube could not be found!');
+            user.deleteCube(option);
+            await user.save();
+            content = `${option} has been deleted!`;
+          }
+        }
+      } catch (error) {
+        if (error.code === 'ERR_INVALID_URL') {
+          content = `URL must be a complete link from cube cobra. Here's an example: https://cubecobra.com/cube/list/thunderwang`
+        }
+        content = error.message;
+      }
+      await interaction.reply({
+        content,
+        ephemeral: true,
+        fetchReply: true,
+        flags: 'SuppressEmbeds'
+      });
+    },
+})
