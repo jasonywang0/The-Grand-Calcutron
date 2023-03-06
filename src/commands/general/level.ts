3c9afe11bdf30c431ec4272426b0202668e8bccd
Jason Wang <jasonywang0@gmail.com>
2023-03-05 18:59:08 -0800

    Fix promise all not working

--- src/commands/general/level.ts
+++ src/commands/general/level.ts
@@ -30,12 +30,12 @@ export default new CommandClass({
         cooldown: 5,
         visible: true,
         guildOnly: true,
-        channels: [process.env.DRAFT_CHANNEL]
+        channels: [process.env.BOT_CHANNEL]
     },
     async execute(interaction: ChatInputCommandInteraction<'cached'>) {
       let ephemeral = true;
       let content = '';
-      const embeds = [];
+      let embeds = [];
       try {
         const subcommand = interaction.options.getSubcommand();
         let user = await User.findUser(interaction.user.id);
@@ -44,32 +44,30 @@ export default new CommandClass({
         switch (subcommand) {
           case 'get': {
             const roles = await Role.findRolesByPoints(points);
-            const highestLevel = roles[0].pop();
-            embeds.push(await createLevelGetEmbed({interaction, points, level: highestLevel}));
+            await interaction.member.roles.add(roles[0].map(role => role.getDiscordId())), // TODO: figure out why I can't put this in Promises.all
+            await interaction.member.roles.remove(roles[1].map(role => role.getDiscordId()))
+            embeds.push(await createLevelGetEmbed({interaction, points, level: roles[0].pop()}));
             break;
           }
           case 'up': {
+            if (!this.opt.channels?.includes(interaction.channelId)) throw new Error('This command can only be used in the bot channel');
             ephemeral = false;
             points += 1;
             user.setPoints(points);
             let roles = await Role.findRolesByPoints(points);
-            await Promise.all([
-              user.save(),
-              interaction.member.roles.add(roles[0].map(role => role.discordId)),
-              interaction.member.roles.remove(roles[1].map(role => role.discordId))
-            ]);
-            const highestLevel = roles[0].pop();
-            embeds.push(await createLevelUpEmbed({interaction, points, level: highestLevel}));
+            await interaction.member.roles.remove(roles[1].map(role => role.getDiscordId()));
+            await interaction.member.roles.add(roles[0].map(role => role.getDiscordId()));
+            const promises = await Promise.all([user.save(), createLevelUpEmbed({interaction, points, level: roles[0].pop()})]);
+            embeds.push(promises[1]);
             break;
           }
           case 'down': {
             points -= 1;
-            let roles = await Role.findRolesByPoints(points);
             user.setPoints(points);
-            await Promise.all([
-              user.save(),
-              interaction.member.roles.remove(roles[1].map(role => role.discordId))
-            ]);
+            let roles = await Role.findRolesByPoints(points);
+            await interaction.member.roles.remove(roles[1].map(role => role.getDiscordId()));
+            await interaction.member.roles.add(roles[0].map(role => role.getDiscordId()));
+            await user.save();
             content = `**<@!${interaction.user.id}> now has ${points} points!**`;
             break;
           }
@@ -79,6 +77,7 @@ export default new CommandClass({
         }
       } catch (error) {
         content = error.message;
+        embeds = [];
       }
       await interaction.reply({
         content,
